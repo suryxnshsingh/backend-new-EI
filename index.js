9e6a8a6de48631b130ee943c2ce44d5ea698f3ae
Pranshu Pandey <pranshu0604@gmail.com>
2025-01-03 23:05:58 +0530

    student side attendance update 3

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ import attendanceRoutes from './routes/attendance.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
